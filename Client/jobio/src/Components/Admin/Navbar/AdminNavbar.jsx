a32e7b6950b41d75ef1fbb45dd5f454974963a61
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-08 16:32:31 +0530

    Update AdminNavbar.jsx

--- Client/jobio/src/Components/Admin/Navbar/AdminNavbar.jsx
+++ Client/jobio/src/Components/Admin/Navbar/AdminNavbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './AdminNavbar.scss'
 import { DiCodeigniter } from "react-icons/di";
+import { IoHome,IoLayers, IoChatbubbleEllipses,IoPeople ,IoCog} from "react-icons/io5";
+
 
 const AdminNavbar = () => {
     return (
@@ -11,6 +13,31 @@ const AdminNavbar = () => {
                         <DiCodeigniter />
                     </div>
                 </header>
+
+                <div className="sidebar-menu">
+                    <div className="menu">
+                        <ul className="menu-links">
+                            <li className="nav-link">
+                                <IoHome />
+                                <span>Home</span>
+                            </li>
+                            <li>
+                                <IoLayers />
+                                <span>Services</span>
+                            </li>
+                            <li>
+                                <IoChatbubbleEllipses />
+                                <span>Inbox</span>
+                            </li>
+                            <li>
+                                <IoPeople/>
+                                <span>Chats</span>
+                            </li>
+                            <li><IoCog/></li>
+                            <span>Settings</span>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
