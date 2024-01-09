f17c77f8c7256b9d95d2fd103733c21c41d2b434
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 15:03:19 +0530

    Update Profile.jsx

--- Client/jobio/src/Components/Profile/Profile.jsx
+++ Client/jobio/src/Components/Profile/Profile.jsx
@@ -3,7 +3,13 @@ import './Profile.scss'
 const Profile = () => {
   return (
     <div className="Profile">
-      
+        <div className="column">
+          <div className="profile-container"></div>
+
+          <div className="education"></div>
+
+          <div className="skills"></div>
+        </div>
     </div>
   )
 }
