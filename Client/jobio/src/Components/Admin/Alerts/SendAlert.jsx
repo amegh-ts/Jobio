b4a8d667a0e4ac77055b406373e658c98c956830
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 19:31:49 +0530

    Update SendAlert.jsx

--- Client/jobio/src/Components/Admin/Alerts/SendAlert.jsx
+++ Client/jobio/src/Components/Admin/Alerts/SendAlert.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
 
 const SendAlert = () => {
+  const [notification, setNotification] = useState('');
+  const [priority, setPriority] = useState('Common Message');
+
+  const handleTextareaChange = (event) => {
+    setNotification(event.target.value);
+  };
+
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const handleSendClick = async () => {
+    try {
+      await sendNotification({ notification, priority })
+    } catch (error) {
+      console.log(error);
+    }
+    alert("Message sent successfully")
+  };
   return (
-    <div>SendAlert</div>
+    <div>
+      <div className="send-notification-main">
+        <div className="send-notification-header">
+          <LuBellPlus className="bell-plus-icon" />
+          <h3>Notification</h3>
+        </div>
+        <div className="send-notification-body">
+          <div>
+            <select value={priority} onChange={handlePriorityChange}>
+              <option value="not-so-much">Common Message</option>
+              <option value="low">Low Priority</option>
+              <option value="medium">Medium Priority</option>
+              <option value="high">High Priority</option>
+            </select>
+          </div>
+          <div>
+            <textarea name="notification" cols="30" rows="10" value={notification} onChange={handleTextareaChange} ></textarea>
+          </div>
+          <div>
+            <button onClick={handleSendClick}>Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
