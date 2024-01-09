d6f1fc4b6187d55b9dc21c84275908d45b0279fc
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 17:24:44 +0530

    09-01-2024

--- Client/jobio/src/Components/Admin/Home/Home.jsx
+++ Client/jobio/src/Components/Admin/Home/Home.jsx
@@ -4,10 +4,20 @@ const Home = () => {
   return (
     <div className="Home">
       <div className="home-main">
-        <div className='left'></div>
+        <div className='left'>
+          <div className='header'>
+            <img src="/Images/bg.png" alt="" />
+          </div>
+          <div className='photo'></div>
+          <div className='footer'>
+            <h2>Name</h2>
+            <h3>Highlights</h3>
+            <h5>kozhikode, kerala, India <span>Contact info</span></h5>
+          </div>
+        </div>
 
         <div className='middle'></div>
-        
+
         <div className='right'></div>
       </div>
     </div>
