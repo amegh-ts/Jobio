16a90345b344c3618fee46429c14453cba46940b
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 15:07:53 +0530

    09-01-2024

--- Client/jobio/src/Components/Profile/Profile.jsx
+++ Client/jobio/src/Components/Profile/Profile.jsx
@@ -3,13 +3,17 @@ import './Profile.scss'
 const Profile = () => {
   return (
     <div className="Profile">
-        <section className="column">
-          <div className="profile-container"></div>
-hhh
-          <div className="education"></div>
+      <section className="column">
+        <div className="profile-container">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
 
-          <div className="skills"></div>
-        </section>
+        <div className="education"></div>
+
+        <div className="skills"></div>
+      </section>
     </div>
   )
 }
