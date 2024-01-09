bd757e53c4ef26da43ca2d9e61818532adb21f66
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 19:40:56 +0530

    09-01-2024

--- Client/jobio/src/Components/Admin/Alerts/ViewAlert.jsx
+++ Client/jobio/src/Components/Admin/Alerts/ViewAlert.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
 
 const ViewAlert = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    async function display() {
+      // const notifications = await getNotification();
+      // setState(notifications);
+    }
+    display()
+
+  }, [])
+
+  const getPriorityColor = (priority) => {
+    switch (priority) {
+      case 'high':
+        return 'high-priority';
+      case 'medium':
+        return 'medium-priority';
+      case 'low':
+        return 'low-priority';
+      default:
+        return '';
+    }
+  };
   return (
-    <div>ViewAlert</div>
+    <div>
+      <div className="notification-main">
+        <div className='notification-header'>
+          <CiBellOn className='bell-icon' />
+          <h3>Admin Notification</h3>
+        </div>
+        <div className='notification-container'>
+          {state &&
+            state.map((notification) => (
+              <div className={`notification-box ${getPriorityColor(notification.priority)}`} key={notification._id}>
+                <div className="ntb-content">
+                  {/* <h5>{notification.user}</h5> */}
+                  <h6>  {new Date(notification.createdAt).toLocaleString()}</h6>
+                  <p>{notification.notification}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
