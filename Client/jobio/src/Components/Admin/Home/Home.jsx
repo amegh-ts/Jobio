0e8e55798059cfb745836ea3e4bfa0655bba2856
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 18:59:49 +0530

    09-01-2024

--- Client/jobio/src/Components/Admin/Home/Home.jsx
+++ Client/jobio/src/Components/Admin/Home/Home.jsx
@@ -1,47 +1,58 @@
-import './Home.scss'
-import { IoCloseCircleOutline } from "react-icons/io5";
-
+import { useState } from 'react';
+import './Home.scss';
+import { IoCloseCircleOutline } from 'react-icons/io5';
 
 const Home = () => {
+  const [showWelcomeContainer, setShowWelcomeContainer] = useState(true);
+
+  const handleCloseWelcomeContainer = () => {
+    setShowWelcomeContainer(false);
+  };
+
+
   return (
     <div className="Home">
       <div className="home-main">
-        <div className='left'>
-          <div className='header'>
+        <div className="left">
+          <div className="header">
             <img src="/Images/bg.png" alt="" />
           </div>
-          <div className='photo'></div>
-          <div className='footer'>
+          <div className="photo"></div>
+          <div className="footer">
             <h2>Name</h2>
             <h3>Highlights</h3>
-            <h5>kozhikode, kerala, India <span>Contact info</span></h5>
+            <h5>
+              kozhikode, kerala, India <span>Contact info</span>
+            </h5>
           </div>
         </div>
 
-        <div className='middle'>
-          <div className="welcome-container">
-            <div className='close'>
-              <IoCloseCircleOutline className='icon'/>
+        <div className="middle">
+          {showWelcomeContainer && (
+            <div className="welcome-container">
+              <div className="close" onClick={handleCloseWelcomeContainer}>
+                <IoCloseCircleOutline className="icon" />
+              </div>
+              <div className="image">
+                <img src="" alt="Profile" />
+              </div>
+              <div className="note">
+                <h1>Welcome back Devil</h1>
+              </div>
+              <div className="status">
+                <button>Available to work</button>
+                <button>Unavailable to work</button>
+              </div>
             </div>
-            <div className='image'>
-              <img src="" alt="Profile" />
-            </div>
-            <div className='note'>
-              <h1>Welcome back Devil</h1>
-            </div>
-            <div className='status'>
-              <button>Available to work</button>
-              <button>Unavailabel to work</button>
-            </div>
-          </div>
+          )}
         </div>
 
-        <div className='right'>
+        <div className="right">
           <h1>ads</h1>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
