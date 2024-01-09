ff2d30d22b7ccad5a45332f3bc1a074963f35bad
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-09 11:41:01 +0530

    09-01-2024

--- Client/jobio/src/App.jsx
+++ Client/jobio/src/App.jsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import AdminNavbar from './Components/Admin/Navbar/AdminNavbar';
+import Index from './Components/Index';
+import ClientNavbar from './Components/Client/Navbar/ClientNavbar';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
+      element:<Index/> ,
+    }, {
+      path: 'anav',
       element:<AdminNavbar/> ,
+    }, {
+      path: 'cnav',
+      element:<ClientNavbar/> ,
     }, 
   ]);
   return (
