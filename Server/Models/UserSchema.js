36aae9fb4d626c4bfd0cff935957a2d7431450fb
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 15:30:52 +0530

    10-01-2024

--- Server/Models/UserSchema.js
+++ Server/Models/UserSchema.js
@@ -1,13 +1,24 @@
 const mongoose=require('mongoose')
 
 const UserSchema=new mongoose.Schema({
-    // username:{type:String,required:true,unique:true},  //require:true is user to make this feild compulsory
     username:{type:String,required:true},
-    dob:{type:String,required:true},
     email:{type:String,unique:true,required:true},
-    phone:{type:String,unique:true,required:true},
+    dob:{type:String,required:true},
+    phone:{type:String,unique:true},
     password:{type:String,required:true},
-    type:{type:String,required:true}
+    type:{type:String,required:true},
+    firstname:{type:String},
+    lastname:{type:String},
+    city:{type:String},
+    district:{type:String},
+    photo:{type:String},
+    coverphoto:{type:String},
+    skills:{type:Array},
+    about:{type:Array},
+    institute:{type:Array},
+    course:{type:Array},
+    end:{type:Array},
+
 },{timestamps:true}) //to add the date and time the value is added to DB or edited
 
 module.exports=mongoose.model("users",UserSchema)  //here 'users' is the name of the collection you are creating
