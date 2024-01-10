2ac07a7a486e3c40327e488a2865bfdabbca0c83
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 13:01:10 +0530

    10-01-2024

--- Client/jobio/src/Components/Login/Login.jsx
+++ Client/jobio/src/Components/Login/Login.jsx
@@ -1,22 +1,25 @@
 import './Login.scss'
 const Login = () => {
-  return (
-    <div>
-        <div className="Login">
-            <div className="login-container">
-                <div className="container-left"></div>
+    return (
+        <div>
+            <div className="Login">
+                <div className="login-container">
+                    <div className="container-left"></div>
                     <div className="image">
                         <img src="/Images/login.png" alt="" />
                     </div>
-                <div className="container-right">
-                    <h3>Member Login</h3>
-                    <input type="text" />
-                    <input type="text" />
+                    <div className="container-right">
+                        <form action="">
+                            <h3>Member Login</h3>
+                            <input type="text" />
+                            <input type="text" />
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
