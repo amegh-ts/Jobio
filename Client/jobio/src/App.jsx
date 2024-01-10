05e922f8f1bc2d7e70db9115e73737923f81d855
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 14:52:10 +0530

    Update App.jsx

--- Client/jobio/src/App.jsx
+++ Client/jobio/src/App.jsx
@@ -5,6 +5,7 @@ import Index from './Components/Index';
 import ClientNavbar from './Components/Client/Navbar/ClientNavbar';
 import Profile from './Components/Profile/Profile';
 import Login from './Components/Login/Login';
+import Signup from './Components/Login/Signup';
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +24,9 @@ function App() {
     }, {
       path: '/login',
       element:<Login/> ,
+    }, {
+      path: '/signup',
+      element:<Signup/> ,
     }, 
   ]);
   return (
