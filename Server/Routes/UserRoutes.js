fd5e2df1008c6c41a6bca1617d28ac050699aad3
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 17:23:16 +0530

    Update UserRoutes.js

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -1,12 +1,12 @@
-const { signUp } = require('../Controller/UserController')
+const { signUp, signIn } = require('../Controller/UserController')
 
 const router = require('express').Router()
 
 // Signup
-router.post('/signup',signUp)
+router.post('/signup', signUp)
 
 //signin
-router.post('/signin' sig)
+router.post('/signin', signIn)
 
 
 module.exports = router
