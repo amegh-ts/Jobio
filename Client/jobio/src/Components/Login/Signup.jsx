1a48c930c6e6c020f90fd4585f96f44aa7280d78
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 16:13:46 +0530

    Update Signup.jsx

--- Client/jobio/src/Components/Login/Signup.jsx
+++ Client/jobio/src/Components/Login/Signup.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        dob: '',
+        type: '',
+        password: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const logFormData = () => {
+        console.log('Form Data:', formData);
+    };
+
     return (
         <div>
             <div className="Signup">
@@ -19,22 +40,22 @@ const Signup = () => {
                                 <input type="text" name="email" placeholder="Email" />
                             </div>
                             <div className="input">
-                            <i className='icon bx bxs-calendar bx-tada' ></i>
+                                <i className='icon bx bxs-calendar bx-tada' ></i>
                                 <input type="date" name="dob" />
                             </div>
                             <div className="input">
-                            <i className='icon bx bxs-calendar bx-tada' ></i>
-                                <select name="type" id="type">
-                                <option value="" disabled selected>Your Purpose</option>
-                                    <option value="type">Find Job</option>
-                                    <option value="type">Hire</option>
+                                <i className='icon bx bxs-calendar bx-tada' ></i>
+                                <select name="type" id="type" onChange={handleInputChange} value={formData.type}>
+                                    <option value="" disabled selected>Your Purpose</option>
+                                    <option value="Find Job">Find Job</option>
+                                    <option value="Hire">Hire</option>
                                 </select>
                             </div>
                             <div className="input">
                                 <i className="icon bx bxs-lock bx-tada"></i>
                                 <input type="password" name="pass" placeholder="Password" />
                             </div>
-                            <button className="signup-form-btn">Sign Up</button>
+                            <button className="signup-form-btn" onClick={logFormData}>Sign Up</button>
 
                             <div className="login-link">
                                 <span>Already have an account? </span>
@@ -46,10 +67,10 @@ const Signup = () => {
                     </div>
 
                     <div className="container-right">
-            <div className="cl-image">
-              <img src="/Images/login.png" alt="" />
-            </div>
-          </div>
+                        <div className="cl-image">
+                            <img src="/Images/login.png" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
