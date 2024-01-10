9b368a2a88062bec711accfbe0dcf40fbc78ba53
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 16:22:17 +0530

    Update Signup.jsx

--- Client/jobio/src/Components/Login/Signup.jsx
+++ Client/jobio/src/Components/Login/Signup.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
+    const [username, setUsername] = useState('')
+    const [dob, setDob] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [type, setType] = useState('')
+
     return (
         <div>
             <div className="Signup">
@@ -12,19 +19,19 @@ const Signup = () => {
                             <span className="signup-form-title">Create an Account</span>
                             <div className="input">
                                 <i className="icon bx bx-user bx-tada"></i>
-                                <input type="text" name="username" placeholder="Username" />
+                                <input type="text" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                             </div>
                             <div className="input">
                                 <i className="icon bx bx-envelope bx-tada"></i>
-                                <input type="text" name="email" placeholder="Email" />
+                                <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
                             <div className="input">
-                            <i className='icon bx bxs-calendar bx-tada' ></i>
-                                <input type="date" name="dob" />
+                                <i className='icon bx bxs-calendar bx-tada' ></i>
+                                <input type="date" name="dob" value={dob} onChange={(e) => setDob(e.target.value)} />
                             </div>
                             <div className="input">
-                            <i className='icon bx bxs-calendar bx-tada' ></i>
-                                <select name="type" id="type">
+                                <i className='icon bx bxs-calendar bx-tada' ></i>
+                                <select name="type" id="type" value={type} onChange={(e) => setType(e.target.value)}>
                                     <option value="" disabled selected>Your Purpose</option>
                                     <option value="Find Job">Find Job</option>
                                     <option value="Hire">Hire</option>
@@ -32,7 +39,7 @@ const Signup = () => {
                             </div>
                             <div className="input">
                                 <i className="icon bx bxs-lock bx-tada"></i>
-                                <input type="password" name="pass" placeholder="Password" />
+                                <input type="password" name="pass" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
                             <button className="signup-form-btn">Sign Up</button>
 
@@ -46,10 +53,10 @@ const Signup = () => {
                     </div>
 
                     <div className="container-right">
-            <div className="cl-image">
-              <img src="/Images/login.png" alt="" />
-            </div>
-          </div>
+                        <div className="cl-image">
+                            <img src="/Images/login.png" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
