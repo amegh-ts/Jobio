5ad25c367984b7bef83905d5e161d9827a1b8cc6
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 13:12:06 +0530

    10-01-2024

--- Client/jobio/src/Components/Login/Login.jsx
+++ Client/jobio/src/Components/Login/Login.jsx
@@ -9,11 +9,49 @@ const Login = () => {
                         <img src="/Images/login.png" alt="" />
                     </div>
                     <div className="container-right">
-                        <form action="">
-                            <h3>Member Login</h3>
-                            <input type="text" />
-                            <input type="text" />
-                        </form>
+                    <form className="login-form">
+					<span className="login-form-title">
+						Member Login
+					</span>
+
+					<div className="wrap-input " data-validate = "Valid email is required: ex@abc.xyz">
+						<input className="input" type="text" name="email" placeholder="Email"/>
+						<span className="focus-input"></span>
+						<span className="symbol-input">
+							<i className="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div className="wrap-input" data-validate = "Password is required">
+						<input className="input" type="password" name="pass" placeholder="Password"/>
+						<span className="focus-input"></span>
+						<span className="symbol-input">
+							<i className="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div className="container-login-form-btn">
+						<button className="login-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div className="text-center p-t-12">
+						<span className="txt1">
+							Forgot
+						</span>
+						<a className="txt2" href="#">
+							Username / Password?
+						</a>
+					</div>
+
+					<div className="text-center p-t-136">
+						<a className="txt2" href="#">
+							Create your Account
+							<i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
                     </div>
                 </div>
             </div>
