7268a07a67eecd382a221b872ce3e6fe8a9fb417
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-10 12:57:00 +0530

    10-01-2024

--- Client/jobio/src/Components/Login/Login.jsx
+++ Client/jobio/src/Components/Login/Login.jsx
@@ -3,7 +3,13 @@ const Login = () => {
   return (
     <div>
         <div className="Login">
-            
+            <div className="login-container">
+                <div className="container-left"></div>
+                    <div className="image">
+                        <img src="/Images/login.png" alt="" />
+                    </div>
+                <div className="container-right"></div>
+            </div>
         </div>
     </div>
   )
