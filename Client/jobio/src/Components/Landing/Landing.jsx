2cea984fccac1a171c5c228e0f1937e4e8d03609
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-11 14:20:17 +0530

    11-01-204

--- Client/jobio/src/Components/Landing/Landing.jsx
+++ Client/jobio/src/Components/Landing/Landing.jsx
@@ -29,12 +29,31 @@ const Landing = () => {
                     <div className="featured">
                         <p>Featured recruiters and employers</p>
                         <div className="featured-images">
-                            <img src="/Images/Svg/logo-coinbase.svg" alt="coinbase" />
-                            <img src="/Images/Svg/logo-nasa.svg" alt="nasa" />
-                            <img src="/Images/Svg/logo-netflix.svg" alt="netflix" />
-                            <img src="/Images/Svg/logo-pinterest.svg" alt="pinterest" />
-                            <img src="/Images/Svg/logo-spotify.svg" alt="spotify" />
-                            <img src="/Images/Svg/logo-vodafone.svg" alt="vodafone" />
+                            <img src="/Images/Landing/Svg/logo-coinbase.svg" alt="coinbase" />
+                            <img src="/Images/Landing/Svg/logo-nasa.svg" alt="nasa" />
+                            <img src="/Images/Landing/Svg/logo-netflix.svg" alt="netflix" />
+                            <img src="/Images/Landing/Svg/logo-pinterest.svg" alt="pinterest" />
+                            <img src="/Images/Landing/Svg/logo-spotify.svg" alt="spotify" />
+                            <img src="/Images/Landing/Svg/logo-vodafone.svg" alt="vodafone" />
+                        </div>
+                    </div>
+
+                    <div className="landing-contents">
+                        <div className="header">
+                            <h1>Why people choose us?</h1>
+                            <p>It really matters and then like it really doesn’t matter. What matters is the people who are sparked by it. And the people who are like offended by it, it doesn’t matter. Because it's about motivating the doers. Because I’m here to follow my dreams and inspire other people to follow their dreams, too.</p>
+                        </div>
+
+                        <div className="img-body">
+                            <div className="lcontainer">
+                                <div className='lc-img'>
+                                    <img src="/Images/Landing/l1.png" alt="" />
+                                </div>
+                                <div className="lc-content">
+                                    <h2>Job Alerts</h2>
+                                    <p>It really matters and then like it really doesn’t matter. What matters is the people who are sparked by it. And the people who are like offended by it, it doesn’t matter. Because it's about motivating the doers. Because I’m here to follow my dreams and inspire other people to follow their dreams, too.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
