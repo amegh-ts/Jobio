20cd86febc851134e0486f138247ac8ae8a3a22c
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-11 12:43:42 +0530

    Update Landing.jsx

--- Client/jobio/src/Components/Landing/Landing.jsx
+++ Client/jobio/src/Components/Landing/Landing.jsx
@@ -1,30 +1,30 @@
 import './Landing.scss'
 
 const Landing = () => {
-  return (
-    <div className="Landing">
-        <header className="landing-header">
-            <div className="title">
-                <h2>Jobio</h2>
-            </div>
-            <div className="header-links">
-                <a href='/login'>Login</a>
-                <a href='/signup'>Register</a>
-            </div>
-        </header>
-        <section className="image-body">
-        <div className="overlay">
-            <h5>It's simple and smart</h5>
-            <h1>Search, Find & Apply</h1>
-            <p>Browse from more than 15,000,000 adverts while new ones come on daily bassis</p>
-            <div className="overlay-button">
-                <span>Register now</span>
-                <button>Register</button>
-            </div>
+    return (
+        <div className="Landing">
+            <header className="landing-header">
+                <div className="title">
+                    <h2>Jobio</h2>
+                </div>
+                <div className="header-links">
+                    <a href='/login'>Login</a>
+                    <a href='/signup'>Register</a>
+                </div>
+            </header>
+            <section className="image-body">
+                <div className="overlay">
+                    <h5>It's simple and smart</h5>
+                    <h1>Search, Find & Apply</h1>
+                    <p>Browse from more than 15,000,000 adverts while new ones come on daily bassis</p>
+                    <div className="overlay-button">
+                        <span>Register now</span>
+                        <button>Register</button>
+                    </div>
+                </div>
+            </section>
         </div>
-        </section>
-    </div>
-  )
+    )
 }
 
 export default Landing
