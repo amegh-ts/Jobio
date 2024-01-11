42854102bf84ef287c35fd2ea91e47718108af22
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-11 12:40:06 +0530

    11-01-2024

--- Client/jobio/src/Components/Landing/Landing.jsx
+++ Client/jobio/src/Components/Landing/Landing.jsx
@@ -8,12 +8,20 @@ const Landing = () => {
                 <h2>Jobio</h2>
             </div>
             <div className="header-links">
-                <span>Login</span>
-                <span>Register</span>
+                <a href='/login'>Login</a>
+                <a href='/signup'>Register</a>
             </div>
         </header>
         <section className="image-body">
-
+        <div className="overlay">
+            <h5>It's simple and smart</h5>
+            <h1>Search, Find & Apply</h1>
+            <p>Browse from more than 15,000,000 adverts while new ones come on daily bassis</p>
+            <div className="overlay-button">
+                <span>Register now</span>
+                <button>Register</button>
+            </div>
+        </div>
         </section>
     </div>
   )
