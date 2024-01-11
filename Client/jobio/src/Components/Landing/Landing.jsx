9c93196a6ea33c2b7b84991815c18f69164f36ed
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-11 12:09:37 +0530

    Update Landing.jsx

--- Client/jobio/src/Components/Landing/Landing.jsx
+++ Client/jobio/src/Components/Landing/Landing.jsx
@@ -3,12 +3,15 @@ const Landing = () => {
   return (
     <div className="Landing">
         <header>
-            <div className="title"></div>
+            <div className="title">
+                <h2>Jobio</h2>
+            </div>
             <div className="header-links">
                 <span>Login</span>
                 <span>Register</span>
             </div>
         </header>
+        <section className="image"></section>
     </div>
   )
 }
