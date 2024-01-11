c05540c26c45bc1a8a239850d58e0cdc0d216eec
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-11 11:39:46 +0530

    Update main.jsx

--- Client/jobio/src/main.jsx
+++ Client/jobio/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux';
+import { store, persistor } from './Redux/store';       //new
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
