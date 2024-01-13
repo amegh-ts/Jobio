1fd3863dda445ac129f066658a388f7a89330320
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 13:10:26 +0530

    13-01-2024

--- Client/jobio/src/Components/Admin/Settings/AdminSettings.jsx
+++ Client/jobio/src/Components/Admin/Settings/AdminSettings.jsx
@@ -1,7 +1,14 @@
-
+import './AdminSettings.scss'
 const AdminSettings = () => {
   return (
-    <div className="Admin-settings">AdminSettings</div>
+    <div className="Admin-settings">
+      <div className="settings-container">
+        <h3>Settings</h3>
+        <div className="content-container">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
