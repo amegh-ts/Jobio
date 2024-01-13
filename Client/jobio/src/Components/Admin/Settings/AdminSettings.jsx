a35e68177c6333f3e2f7142560cb4d0fbceaac53
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 13:18:41 +0530

    13-01-2024

--- Client/jobio/src/Components/Admin/Settings/AdminSettings.jsx
+++ Client/jobio/src/Components/Admin/Settings/AdminSettings.jsx
@@ -1,15 +1,27 @@
-import './AdminSettings.scss'
+import './AdminSettings.scss';
+
 const AdminSettings = () => {
   return (
     <div className="Admin-settings">
       <div className="settings-container">
         <h3>Settings</h3>
         <div className="content-container">
-          
+          <div className="content-left">
+            <div className="left-option" title="Edit Profile">
+              edit Profile
+            </div>
+            <div className="left-option" title="Delete Profile">
+              Delete Profile
+            </div>
+            {/* Add more options as needed */}
+          </div>
+          <div className="content-right">
+            <p className="description">Hover over left options for details</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminSettings
+export default AdminSettings;
