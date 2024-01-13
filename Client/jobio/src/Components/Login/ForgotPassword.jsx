740f62e46a47591707ed51f6161defcbcd9f2417
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 14:26:10 +0530

    Update ForgotPassword.jsx

--- Client/jobio/src/Components/Login/ForgotPassword.jsx
+++ Client/jobio/src/Components/Login/ForgotPassword.jsx
@@ -14,27 +14,18 @@ const ForgotPassword = () => {
                     <div className="container-right">
                         <form className="login-form">
                             <span className="login-form-title">
-                                Member Login
+                                Forgot Password?
                             </span>
                             <>
                             <div className='input'>
                             <i className='icon bx bx-envelope bx-tada' ></i>
-                                <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </div>
-                            <div className='input'>
-                            <i className='icon bx bxs-lock bx-tada' ></i>
-                                <input type="password" name="pass" placeholder="Password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                {/* <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /> */}
                             </div>
                             </>
-                            <button className="login-form-btn" onClick={onLogInClick}>Login</button>
-
-                            <div className="forget-pass">
-                                <span>Forgot </span>
-                                <a className="txt" href="/forgotpassword"> Password? </a>
-                            </div>
+                            {/* <button className="login-form-btn" onClick={onLogInClick}>Login</button> */}
 
                             <div className="create-account">
-                                <a className="txt" href="/signup">Create your Account </a>
+                                <a className="txt" href="/signup">Login</a>
                             </div>
                         </form>
                     </div>
