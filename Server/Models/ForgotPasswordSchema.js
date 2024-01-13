5be58dce404e0b47fcbcf916605cb7f9f17a7de8
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 14:39:05 +0530

    Update ForgotPasswordSchema.js

---
+++ Server/Models/ForgotPasswordSchema.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+const ForgotPasswordSchema=new mongoose.Schema({
+    email:{type:String,required:true,unique:true},
+    otp:{type:String,required:true},
+    otpExpiration:{type:Date,required:true}
+})
+
+module.exports=mongoose.model('Forgot',ForgotPasswordSchema)
+
