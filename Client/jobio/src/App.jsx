73c60c6764dd013e6d469df8ab7f37918e14e604
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 12:57:41 +0530

    Update App.jsx

--- Client/jobio/src/App.jsx
+++ Client/jobio/src/App.jsx
@@ -9,8 +9,36 @@ import Signup from './Components/Login/Signup';
 import { useSelector } from 'react-redux';
 import Landing from './Components/Landing/Landing';
 import EmployerNavbar from './Components/Employer/Navbar/EmployerNavbar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [primaryColor, setPrimaryColor] = useState('');
+
+const storedData = localStorage.getItem('persist:jobio');
+const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
+const userType = user?.userInfo?.[0]?.type;
+
+
+
+  useEffect(() => {
+    // Set primary color based on user type
+    switch (userType) {
+      case 'admin':
+        setPrimaryColor('rgb(231, 0, 0)'); // Red color
+        break;
+      case 'employer':
+        setPrimaryColor('rgb(0, 128, 0'); // Green color
+        break;
+      case 'employee':
+        setPrimaryColor('#695CFE'); // Blue color
+        break;
+      default:
+        setPrimaryColor('#695CFE'); // Default color
+    }
+    document.body.style.setProperty('--primary-color', primaryColor);
+
+  }, [primaryColor]);
+
 
   const reduxData = useSelector((state) => state.user.userInfo[0]);
   // console.log('reduxdata', reduxData);
