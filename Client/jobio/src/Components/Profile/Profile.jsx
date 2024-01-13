cb3aa650474f2df29853c5081c10386036db67f2
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 12:59:46 +0530

    13-01-2024

--- Client/jobio/src/Components/Profile/Profile.jsx
+++ Client/jobio/src/Components/Profile/Profile.jsx
@@ -26,6 +26,33 @@ const Profile = () => {
   const [course, setCourse] = useState('')
   const [year, setYear] = useState('')
 
+  const [primaryColor, setPrimaryColor] = useState('');
+
+  const storedData = localStorage.getItem('persist:jobio');
+  const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
+  const userType = user?.userInfo?.[0]?.type;
+
+
+
+  useEffect(() => {
+    // Set primary color based on user type
+    switch (userType) {
+      case 'admin':
+        setPrimaryColor('rgb(231, 0, 0)'); // Red color
+        break;
+      case 'employer':
+        setPrimaryColor('rgb(0, 128, 0'); // Green color
+        break;
+      case 'employee':
+        setPrimaryColor('#695CFE'); // Blue color
+        break;
+      default:
+        setPrimaryColor('#695CFE'); // Default color
+    }
+    document.body.style.setProperty('--primary-color', primaryColor);
+
+  }, [primaryColor]);
+
 
 
   const [selectedSkills, setSelectedSkills] = useState([]);
