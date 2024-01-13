56d540bdaf36c7ba5bc757c636329893ae295950
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 13:47:37 +0530

    Update Popup.jsx

--- Client/jobio/src/Assets/Popups/Popup.jsx
+++ Client/jobio/src/Assets/Popups/Popup.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
 import './Popup.scss'
 import { IoCloseCircle } from "react-icons/io5";
 
 const Popup = (props) => {
+    
+  const [primaryColor, setPrimaryColor] = useState('');
+
+  const storedData = localStorage.getItem('persist:jobio');
+  const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
+  const userType = user?.userInfo?.[0]?.type;
+
+
+
+  useEffect(() => {
+    // Set primary color based on user type
+    switch (userType) {
+      case 'admin':
+        setPrimaryColor('rgb(231, 0, 0)'); // Red color
+        break;
+      case 'employer':
+        setPrimaryColor('rgb(0, 128, 0'); // Green color
+        break;
+      case 'employee':
+        setPrimaryColor('#695CFE'); // Blue color
+        break;
+      default:
+        setPrimaryColor('#695CFE'); // Default color
+    }
+    document.body.style.setProperty('--primary-color', primaryColor);
+
+  }, [primaryColor]);
+
+
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
