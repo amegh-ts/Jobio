1ec2c3b90a5338c61f4771d7a08a243c2e25074f
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-13 14:52:39 +0530

    13-01-2024

--- Client/jobio/src/Components/Login/ForgotPassword.jsx
+++ Client/jobio/src/Components/Login/ForgotPassword.jsx
@@ -4,14 +4,16 @@ import Popup from '../../Assets/Popups/Popup';
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('')
+    const [forgotPassPopup, setForgotPassPopup] = useState(false)
 
 
-    const onForgotPassClick=async(e)=>{
+    const onForgotPassClick = async (e) => {
         e.preventDefault();
+        setForgotPassPopup(true)
     }
-  return (
-    <div>
-        <div className="Forgot">
+    return (
+        <div>
+            <div className="Forgot">
                 <div className="login-container">
                     <div className="container-left">
                         <div className="cl-image">
@@ -25,15 +27,36 @@ const ForgotPassword = () => {
                                 Forgot Password?
                             </span>
                             <>
-                            <div className='input'>
-                            <i className='icon bx bx-envelope bx-tada' ></i>
-                                <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </div>
+                                <div className='input'>
+                                    <i className='icon bx bx-envelope bx-tada' ></i>
+                                    <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                </div>
                             </>
                             <button className="login-form-btn" onClick={onForgotPassClick}>Send OTP</button>
 
-                            <Popup>
-                                <div className="forgotpass-popup"></div>
+                            <Popup trigger={forgotPassPopup} setTrigger={setForgotPassPopup}>
+                                <div className="forgotpass-popup">
+                                    <h3>OTP</h3>
+                                    <div className='forgotpass-container'>
+                                        {/* Section 1: Prompt to enter OTP */}
+                                        <div className='otp-prompt'>
+                                            <span>Enter OTP:</span>
+                                        </div>
+
+                                        {/* Section 2: Input boxes for OTP */}
+                                        <div className='otp-input'>
+                                            {Array.from({ length: 6 }).map((_, index) => (
+                                                <input
+                                                    key={index}
+                                                    type="text"
+                                                    maxLength="1"
+                                                    value={otp[index] || ''}
+                                                    onChange={onOtpInputChange}
+                                                />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
                             </Popup>
 
                             <div className="create-account">
@@ -43,8 +66,8 @@ const ForgotPassword = () => {
                     </div>
                 </div>
             </div >
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ForgotPassword
