5212e4d667689a11b9654ca9b92e479472b4ae1c
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-15 16:37:21 +0530

    Update UserController.js

--- Server/Controller/UserController.js
+++ Server/Controller/UserController.js
@@ -11,7 +11,7 @@ const signUp = async (req, res) => {
     try {
         console.log('*************************');
         const savedUser = await newUser.save()
-        console.log('saved user',savedUser);
+        console.log('saved user', savedUser);
         res.status(200).json(savedUser)
     } catch (error) {
         res.status(500).json(error)
