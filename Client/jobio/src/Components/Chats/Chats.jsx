7f9204f10549689177625d24a17ee30c3f5a46be
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-15 15:26:18 +0530

    Update Chats.jsx

--- Client/jobio/src/Components/Chats/Chats.jsx
+++ Client/jobio/src/Components/Chats/Chats.jsx
@@ -7,7 +7,7 @@ const Chats = () => {
     const [selectedUser, setSelectedUser] = useState(null);
     const [showChatBody, setShowChatBody] = useState(false);
 
-    // const storedData = localStorage.getItem('persist:unknown');
+    // const storedData = localStorage.getItem('persist:jobio');
     // const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
     // const senderId = user?.userInfo?.[0]?.id;
 
