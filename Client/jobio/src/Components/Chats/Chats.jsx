0b9d88b79eb43049b50852a921be85c6a8ba9647
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-15 17:16:21 +0530

    Update Chats.jsx

--- Client/jobio/src/Components/Chats/Chats.jsx
+++ Client/jobio/src/Components/Chats/Chats.jsx
@@ -13,6 +13,30 @@ const Chats = () => {
     const storedData = localStorage.getItem('persist:jobio');
     const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
     const senderId = user?.userInfo?.[0]?.id;
+    const userType = user?.userInfo?.[0]?.type;
+
+
+
+    useEffect(() => {
+        // Set primary color based on user type
+        switch (userType) {
+            case 'admin':
+                setPrimaryColor('rgb(231, 0, 0)'); // Red color
+                break;
+            case 'employer':
+                setPrimaryColor('rgb(0, 128, 0'); // Green color
+                break;
+            case 'employee':
+                setPrimaryColor('#695CFE'); // Blue color
+                break;
+            default:
+                setPrimaryColor('#695CFE'); // Default color
+        }
+        document.body.style.setProperty('--primary-color', primaryColor);
+
+    }, [primaryColor]);
+
+
 
     useEffect(() => {
         async function fetchData() {
