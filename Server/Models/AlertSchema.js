43cd154c69cf62a786bf5b2a910324df2e61c8ff
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-17 19:02:10 +0530

    Update AlertSchema.js

--- Server/Models/AlertSchema.js
+++ Server/Models/AlertSchema.js
@@ -4,6 +4,6 @@ const AlertSchema=new mongoose.Schema({
     user:{type:String,required:true},
     priority:{type:String,required:true},
     alert:{type:String,required:true}
-})
+},{timestamps:true})
 
 module.exports=mongoose.model("alerts",AlertSchema)
