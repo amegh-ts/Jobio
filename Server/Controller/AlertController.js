6ea9acd09b1af4c0c407ce5c805b094782e74d0b
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-17 18:00:29 +0530

    Update AlertController.js

--- Server/Controller/AlertController.js
+++ Server/Controller/AlertController.js
@@ -1,4 +1,4 @@
-const alert=require('../Models/AlertSchema')
+const alert = require('../Models/AlertSchema')
 
 const sendAlert = async (req, res) => {
     const newAlert = new notification(req.body)
@@ -10,6 +10,11 @@ const sendAlert = async (req, res) => {
     }
 }
 
-const getAlert=async(req,res)=>{
-    
+const getAlert = async (req, res) => {
+    try {
+        const data = await notification.find()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
