4162b51b2d70198fc4e2bf5cee5fb9c7c6b89403
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-17 21:36:57 +0530

    17-01-2024

--- Client/jobio/src/Components/Admin/Home/Home.jsx
+++ Client/jobio/src/Components/Admin/Home/Home.jsx
@@ -1,7 +1,18 @@
 import './Home.scss';
+import { LineChart } from '@mui/x-charts/LineChart';
+
 
 const Home = () => {
-
+  const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+  const xLabels = [
+    'Page A',
+    'Page B',
+    'Page C',
+    'Page D',
+    'Page E',
+    'Page F',
+    'Page G',
+  ];
 
 
 
@@ -10,7 +21,19 @@ const Home = () => {
       <h3>Dashboard</h3>
       <div className="home-main">
         <div className="home-top">
-          <div className="htl">gg</div>
+          <div className="htl">
+            <LineChart
+              width={500}
+              height={300}
+              series={[{ data: uData, label: 'uv', area: true, showMark: false }]}
+              xAxis={[{ scaleType: 'point', data: xLabels }]}
+              sx={{
+                '.MuiLineElement-root': {
+                  display: 'none',
+                },
+              }}
+            />
+          </div>
           <div className="htr">rr</div>
         </div>
       </div>
