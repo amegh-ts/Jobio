59a129cd7ebdcd60a37f662f3586d8652cd79885
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-18 14:12:54 +0530

    Update BanController.js

--- Server/Controller/BanController.js
+++ Server/Controller/BanController.js
@@ -2,16 +2,24 @@ const userController = require('../Models/UserSchema')
 
 
 // fetch user
-const fetchUser=async(req,res)=>{
+const fetchUser = async (req, res) => {
     try {
-        const user= await userController.findById(req.params.id)
+        const user = await userController.findById(req.params.id)
         res.status(200).json(user)
     } catch (error) {
         res.status(500).json(error)
     }
 }
 
-// update user
+// ban/unban user
+const banUser = async (req, res) => {
+    try {
+        const updateData = await userController.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updateData)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 
 
-module.exports={fetchUser}
+module.exports = { fetchUser,banUser }
