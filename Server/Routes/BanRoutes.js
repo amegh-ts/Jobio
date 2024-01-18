cf6fe971d1969b1a3e233aaf6468389a3fe93080
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-18 14:19:05 +0530

    Update BanRoutes.js

--- Server/Routes/BanRoutes.js
+++ Server/Routes/BanRoutes.js
@@ -1,8 +1,11 @@
-const { fetchUser } = require('../Controller/BanController')
+const { fetchUser, banUser } = require('../Controller/BanController')
 
 const router = require('express').Router()
 
 // fetch user
 router.get('/fetchuser/:id',fetchUser)
+
+// ban user
+router.post('/banuser/:id',banUser)
 
 module.exports = router
