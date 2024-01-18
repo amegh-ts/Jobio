d883966b5aba7ccc3f04858527993cee3478feb1
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-18 13:26:24 +0530

    Update BanController.js

--- Server/Controller/BanController.js
+++ Server/Controller/BanController.js
@@ -2,4 +2,15 @@ const userController = require('../Models/UserSchema')
 
 
 // fetch user
-const fetchUser=async
+const fetchUser=async(req,res)=>{
+    try {
+        const user= await userController.findById(req.body._id)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// update user
+
+
+module.exports={fetchUser}
