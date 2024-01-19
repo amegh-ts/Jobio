2202142541cce50beec5dfaa1a4766c17f5aee15
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-19 17:19:51 +0530

    Update FeedSchema.js

--- Server/Models/FeedSchema.js
+++ Server/Models/FeedSchema.js
@@ -3,7 +3,6 @@ const mongoose=require('mongoose')
 const FeedSchema=new mongoose.Schema({
     senderId:{type:String,required:true},
     image:{type:String},
-    title:{type:String,required:true},
     description:{type:String,required:true},
     likes:{type:Number,required:true},
     report:{type:Number,required:true},
