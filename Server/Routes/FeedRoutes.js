f2d07551e9739faa7d9b032b5f9eda7b0f0a35f0
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 00:41:37 +0530

    Update FeedRoutes.js

--- Server/Routes/FeedRoutes.js
+++ Server/Routes/FeedRoutes.js
@@ -1,7 +1,10 @@
-const { AddFeed } = require('../Controller/FeedController')
+const { AddFeed, allFeeds } = require('../Controller/FeedController')
 
 const router = require('express').Router()
 
-router.post('/createfeed',AddFeed)
+// create feed
+router.post('/createfeed',AddFeed);
+// all feeds
+router.get('/allfeeds',allFeeds);
 
 module.exports = router
