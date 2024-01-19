f92a09715a9c35d5a7785e8281e56e6260f926ae
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 00:28:36 +0530

    Update FeedController.js

--- Server/Controller/FeedController.js
+++ Server/Controller/FeedController.js
@@ -1,12 +1,18 @@
-const FeedSchema=require('../Models/FeedSchema')
+const FeedSchema = require('../Models/FeedSchema')
 
 // create feed
-const AddFeed=async(req,res)=>{
+const AddFeed = async (req, res) => {
     console.log(req.body);
+    const feedData = new FeedSchema(req.body)
+    try {
+        await feedData.save();
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 // view all feed
 // view feeds
 // edit feed
 // delete feed
 
-module.exports={AddFeed}
+module.exports = { AddFeed }
