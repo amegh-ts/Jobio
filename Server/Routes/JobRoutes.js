dfa54a0f2277bd71edb73bb538a1abf7640272b1
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 16:06:01 +0530

    Update JobRoutes.js

--- Server/Routes/JobRoutes.js
+++ Server/Routes/JobRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
 // add jobs
+router.post('/addjob',)
 // view all jobs
 // view jobs by user id
 // edit jobs
 // delete jobs 
+
+
+module.exports = router
