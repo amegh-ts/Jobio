e0022bbb16624b2fdf48ad7713ed13646b3ecbfa
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 17:34:42 +0530

    20-01-2024

--- Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
+++ Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
@@ -9,9 +9,31 @@ const Jobs = () => {
                 <button>New Feed</button>
             </div>
             <div className="jobs-body">
-                <div className="jb-left"></div>
+                <div className="jb-left">
+                    <div className="jbl-container">
+                        <div className="jblc-cards">
+                            <h1>Job Title</h1>
+                            <h1>place</h1>
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                           
+                        </div>
 
-                <div className="jb-right"></div>
+                        <div className="jblc-cards">
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                            <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                           
+                        </div>
+                    </div>
+                </div>
+
+                <div className="jb-right">
+                    <div className="jbr-header">
+                        <h1>Create Feed</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
