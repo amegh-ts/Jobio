fed27f27b4636ac8ac86afe1952fb4987b38fc10
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 16:04:36 +0530

    Update JobController.js

--- Server/Controller/JobController.js
+++ Server/Controller/JobController.js
@@ -1,7 +1,14 @@
 const JobSchema=require('../Models/JobSchema')
 
 // add jobs
-
+const AddJobs = async (req, res) => {
+    const feedData = new JobSchema(req.body)
+    try {
+        await feedData.save();
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 // view all jobs
 // view jobs by user id
 // edit jobs
