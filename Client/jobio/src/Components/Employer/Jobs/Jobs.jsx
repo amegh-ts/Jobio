4f5e10248052504813fa8cf2b6a27073b8c75946
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-20 17:56:54 +0530

    20-02-2024

--- Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
+++ Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
@@ -11,6 +11,7 @@ const Jobs = () => {
                 <div className="jobs-body">
                     <div className="jb-left">
                         <div className="jbl-container">
+
                             <div className="jblc-cards">
                                 <div className="jbc-header">
                                     <h3>Job Title</h3>
@@ -22,12 +23,11 @@ const Jobs = () => {
 
                                 <div className="jbc-body">
                                     <p>Description</p>
-                                    <>
+                                    <span>
                                         <button>Skill 1</button>
                                         <button>Skill 2</button>
-                                    </>
+                                    </span>
                                 </div>
-
                                 <div className="jbc-footer">
                                     <h6>Date</h6>
                                     <button>Apply Now</button>
@@ -35,18 +35,81 @@ const Jobs = () => {
                             </div>
 
                             <div className="jblc-cards">
-                                <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
-                                <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
-                                <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
-                                <h1>helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</h1>
+                                <div className="jbc-header">
+                                    <h3>Job Title</h3>
+                                    <h4>place</h4>
+                                    <div>
+                                        <button>Revenue</button>
+                                    </div>
+                                </div>
 
+                                <div className="jbc-body">
+                                    <p>Description</p>
+                                    <span>
+                                        <button>Skill 1</button>
+                                        <button>Skill 2</button>
+                                    </span>
+                                </div>
+                                <div className="jbc-footer">
+                                    <h6>Date</h6>
+                                    <button>Apply Now</button>
+                                </div>
                             </div>
+
+                            <div className="jblc-cards">
+                                <div className="jbc-header">
+                                    <h3>Job Title</h3>
+                                    <h4>place</h4>
+                                    <div>
+                                        <button>Revenue</button>
+                                    </div>
+                                </div>
+
+                                <div className="jbc-body">
+                                    <p>Description</p>
+                                    <span>
+                                        <button>Skill 1</button>
+                                        <button>Skill 2</button>
+                                    </span>
+                                </div>
+                                <div className="jbc-footer">
+                                    <h6>Date</h6>
+                                    <button>Apply Now</button>
+                                </div>
+                            </div>
+
+                            <div className="jblc-cards">
+                                <div className="jbc-header">
+                                    <h3>Job Title</h3>
+                                    <h4>place</h4>
+                                    <div>
+                                        <button>Revenue</button>
+                                    </div>
+                                </div>
+
+                                <div className="jbc-body">
+                                    <p>Description</p>
+                                    <span>
+                                        <button>Skill 1</button>
+                                        <button>Skill 2</button>
+                                    </span>
+                                </div>
+                                <div className="jbc-footer">
+                                    <h6>Date</h6>
+                                    <button>Apply Now</button>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-
                     <div className="jb-right">
                         <div className="jbr-header">
                             <h1>Create Feed</h1>
+                        </div>
+                        <div className="jbr-body">
+                            <div className="jbrb-container">
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
