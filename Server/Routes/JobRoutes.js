73aaabe0c9a3792a5579bb737811f6b6a4a43a83
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-22 15:01:43 +0530

    22-01-2024

--- Server/Routes/JobRoutes.js
+++ Server/Routes/JobRoutes.js
@@ -1,14 +1,15 @@
-const { AddJobs } = require('../Controller/JobController')
+const { AddJobs, getAllJobs, jobsById, deleteJobs } = require('../Controller/JobController')
 const { verifyToken, verifyTokenAndAuthorization } = require('../VerifyToken')
 
 const router = require('express').Router()
 
 // add jobs
-router.post('/addjob',verifyToken, AddJobs)
+router.post('/addjob',verifyToken, AddJobs);
 // view all jobs
+router.get('/alljobs',verifyToken,getAllJobs);
 // view jobs by user id
-// edit jobs
+router.get('/jobById',verifyToken,verifyTokenAndAuthorization,jobsById);
 // delete jobs 
-
+router.delete('/deletejob',verifyToken,deleteJobs);
 
 module.exports = router
