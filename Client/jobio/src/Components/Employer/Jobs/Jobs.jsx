f5208ec12ad277562faa99af50c7c09193ca5e69
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-22 17:10:23 +0530

    Update Jobs.jsx

--- Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
+++ Client/jobio/src/Components/Employer/Jobs/Jobs.jsx
@@ -81,50 +81,6 @@ const Jobs = (props) => {
                     <div className="jb-left">
                         <div className="jbl-container">
 
-                            <div className="jblc-cards">
-                                <div className="jbc-header">
-                                    <h3>Job Title</h3>
-                                    <h4>place</h4>
-                                    <div>
-                                        <button>Revenue</button>
-                                    </div>
-                                </div>
-
-                                <div className="jbc-body">
-                                    <p>Description</p>
-                                    <span>
-                                        <button>Skill 1</button>
-                                        <button>Skill 2</button>
-                                    </span>
-                                </div>
-                                <div className="jbc-footer">
-                                    <h6>Date</h6>
-                                    <button>Delete</button>
-                                </div>
-                            </div>
-                            <div className="jblc-cards">
-                                <div className="jbc-header">
-                                    <h3>Job Title</h3>
-                                    <h4>place</h4>
-                                    <div>
-                                        <button>Revenue</button>
-                                    </div>
-                                </div>
-
-                                <div className="jbc-body">
-                                    <p>Description</p>
-                                    <span>
-                                        <button>Skill 1</button>
-                                        <button>Skill 2</button>
-                                    </span>
-                                </div>
-                                <div className="jbc-footer">
-                                    <h6>Date</h6>
-                                    <button>Delete</button>
-                                </div>
-                            </div>
-
-
                             {reversedJobs && reversedJobs.map((jobs)=>(
                             <div className="jblc-cards" key={jobs._id}>
                                 <div className="jbc-header">
