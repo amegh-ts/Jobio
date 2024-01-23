766db7aac557fbd2ab5f73de27ac4a171944c202
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-23 19:02:31 +0530

    Update ViewJobs.jsx

--- Client/jobio/src/Components/Client/Jobs/ViewJobs.jsx
+++ Client/jobio/src/Components/Client/Jobs/ViewJobs.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import './ViewJobs.scss'
+import { AllJobs } from '../../ApiCalls';
 
 const ViewJobs = () => {
+    const [jobs, setJobs] = useState('')
+
+
+    useEffect(() => {
+        async function fetchJobs() {
+            try {
+                const jobData = await AllJobs();
+                // console.log(jobData);
+                setJobs(jobData)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchJobs()
+    }, [])
+
+    const reversedJobs = [...jobs].reverse();
+
+
     return (
         <div className='ViewJobs'>
             <div className="vj-main">
@@ -11,19 +32,23 @@ const ViewJobs = () => {
                     <div className="vjmb-left">
                         <div className="vjml-container">
 
+                            {reversedJobs && reversedJobs.map((job) => {
 
-                            <div className="vjml-card">
-                                <div className="vjmlc-header">
-                                    <h3>Python Fullstack</h3>
-                                    <h5>userid</h5>
-                                </div>
-                                <div className="vjmlc-body">
+                                <div className="vjml-card">
+                                    <div className="vjmlc-header">
+                                        <h3>Python Fullstack</h3>
+                                        <h5>userid</h5>
+                                    </div>
+                                    <div className="vjmlc-body">
 
+                                    </div>
+                                    <div className="vjmlc-footer">
+                                        <h6>date</h6>
+                                    </div>
                                 </div>
-                                <div className="vjmlc-footer">
-                                    <h6>date</h6>
-                                </div>
-                            </div>
+                            })}
+
+
 
                             <div className="vjml-card">
                                 <div className="vjmlc-header">
