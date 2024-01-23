9083b4a0a523ed0cd06d117bbca45485382db7d2
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-23 18:41:25 +0530

    Update ViewJobs.jsx

--- Client/jobio/src/Components/Client/Jobs/ViewJobs.jsx
+++ Client/jobio/src/Components/Client/Jobs/ViewJobs.jsx
@@ -1,19 +1,34 @@
 import './ViewJobs.scss'
 
 const ViewJobs = () => {
-  return (
-    <div className='ViewJobs'>
-        <div className="vj-main">
-            <div className="vjm-header">
-                <h3>Job Openings</h3>
-            </div>
-            <div className="vjm-body">
-                <div className="vjmb-left"></div>
-                <div className="vjmb-right"></div>
+    return (
+        <div className='ViewJobs'>
+            <div className="vj-main">
+                <div className="vjm-header">
+                    <h3>Job Openings</h3>
+                </div>
+                <div className="vjm-body">
+                    <div className="vjmb-left">
+                        <div className="vjml-container">
+                            <div className="vjml-card">
+                                <div className="vjmlc-header">
+                                    <h3>Python Fullstack</h3>
+                                    <h5>userid</h5>
+                                </div>
+                                <div className="vjmlc-boady">
+
+                                </div>
+                                <div className="vjmlc-footer">
+                                    <h6>date</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="vjmb-right"></div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ViewJobs
