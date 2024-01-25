b0583c10383a547c6f31c35458b734b75f23fc46
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-25 11:18:33 +0530

    Update ApplicationSchema.js

--- Server/Models/ApplicationSchema.js
+++ Server/Models/ApplicationSchema.js
@@ -1,7 +1,12 @@
 const mongoose=require('mongoose')
 
 const ApplicationSchema=new mongoose.Schema({
-    
+    senderId:{type:String,required:true},
+    applicantId:{type:String,required:true},
+    applicantName:{type:String,required:true},
+    jobId:{type:String,required:true},
+    job:{type:String,required:true},
+    senderId:{type:String,required:true},
 })
 
 
