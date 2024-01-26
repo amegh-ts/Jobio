9210e53656f85a0930b805877dfaf71bd5ff95e6
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-27 04:47:39 +0530

    27-01-2024

--- Client/jobio/src/Components/Client/Entries/Entries.jsx
+++ Client/jobio/src/Components/Client/Entries/Entries.jsx
@@ -8,23 +8,23 @@ const Entries = () => {
                     <h3>My Entries</h3>
                 </div>
                 <div className="em-body">
-                    <div className="emb-table">
-                        <table id="detailTable">
-                            <thead>
-                                <tr>
-                                    <th>Item</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div className="emb-left">
+                        <div className="embl-container">
+                            <div className="embl-card">
+                                <h1>iufhidg</h1>
+                            </div>
+
+                            <div className="embl-card">
+                                <h1>iufhidg</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="emb-right">
+                        <div className="embr-container">
+                            <div className="detailed-info">
+                                
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
