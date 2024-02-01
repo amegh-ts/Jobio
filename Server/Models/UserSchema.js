55a05c718f1cfa735a0d0e24fe792a1b37ac21d7
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-01 17:54:22 +0530

    01-02-2024

--- Server/Models/UserSchema.js
+++ Server/Models/UserSchema.js
@@ -1,28 +1,28 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const UserSchema=new mongoose.Schema({
-    username:{type:String,required:true},
-    email:{type:String,unique:true,required:true},
-    dob:{type:String,required:true},
-    phone:{type:String},
-    password:{type:String,required:true},
-    type:{type:String,required:true},
-    firstname:{type:String},
-    lastname:{type:String},
-    city:{type:String},
-    district:{type:String},
-    photo:{type:String},
-    coverphoto:{type:String},
-    selectedSkills:{type:Array},
-    about:{type:String},
-    institute:{type:String},
-    course:{type:String},
-    year:{type:String},
-    state:{type:String},
-    lastLogin:{type:String},
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    dob: { type: String, required: true },
+    password: { type: String, required: true },
+    type: { type: String, required: true },
+    phone: { type: String },
+    firstname: { type: String },
+    lastname: { type: String },
+    city: { type: String },
+    district: { type: String },
+    photo: { type: String },
+    coverphoto: { type: String },
+    selectedSkills: { type: Array },
+    about: { type: String },
+    institute: { type: String },
+    course: { type: String },
+    year: { type: String },
+    state: { type: String },
+    lastLogin: { type: String },
     // lastLogin:{type:Date,default:()=>Date.now()}
-    
 
-},{timestamps:true}) //to add the date and time the value is added to DB or edited
 
-module.exports=mongoose.model("users",UserSchema)  //here 'users' is the name of the collection you are creating
+}, { timestamps: true }) //to add the date and time the value is added to DB or edited
+
+module.exports = mongoose.model("users", UserSchema)  //here 'users' is the name of the collection you are creating
